1312fa7fa71741f71ede9ba168f19063d3580ff1
haruki7049 <tontonkirikiri@gmail.com>
2024-05-10 17:14:10 +0900

    chore: hoge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
+
+#[derive(Debug, Parser)]
+#[command(version, about, author)]
+struct Args {
+    #[arg(long, default_value_t = String::from(""))]
+    ip_address: String,
+
+    #[arg(long, default_value_t = String::from(""))]
+    subnet_mask: String,
+
+    #[arg(long, default_value_t = String::from(""))]
+    prefix_length: String,
+}
+
